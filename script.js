aa27ce090a0f5bc1389a8f68fa6c1a376d8d93fa
Dev-id-A <davidaranzana618@gmail.com>
2025-06-16 12:30:16 +0200

    Responsive map

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 //Variables
-    const width = 1000;
-    const height = 700;
-    const padding = 200;
+    const width = 960;
+    const height = 600;
 
 //Svg created 
 
     const svg = d3.select("body")
                     .append("svg")
-                    .attr("width", width)
-                    .attr("height", height)
-                    .attr("transform", `translate(${width/3} ${padding/5})`);
+                    .attr("viewBox", `0 0 ${width} ${height}`)
+                    .attr("preserveAspectRatio", "xMidYMid meet")
+                    .style("width", "100%")
+                    .style("height", "100%");
+
 //Map fetched
-
-
     d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json")
         .then(us =>{
             try{
             const counties = topojson.feature(us, us.objects.counties).features;
-
 
 //Append map to svg
                 svg.selectAll("path")
@@ -28,7 +26,9 @@
 //Styling map
                     .attr("fill", "white")
                     .attr("stroke", "black")
-                    .attr("stroke-width", 0.2);
+                    .attr("stroke-width", 0.2)
+                    .attr("transform", "translate(100, 10) scale(0.8)");
+
         }
             catch(error){
             window.alert("There was an error trying to obtain data.");
