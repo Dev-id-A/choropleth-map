5be76c87fc0eca543847dcb22a5f5cbbcf61fcf5
Dev-id-A <davidaranzana618@gmail.com>
2025-06-16 13:41:41 +0200

    Data education added

--- script.js
+++ script.js
@@ -11,11 +11,27 @@
                     .style("width", "100%")
                     .style("height", "100%");
 
+
+
 //Map fetched
-    d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json")
-        .then(us =>{
+Promise.all([
+    d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json"),
+    d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json")
+]).then(([us, eData]) =>{
             try{
             const counties = topojson.feature(us, us.objects.counties).features;
+
+
+//Data education 
+            const minEData = d3.min(eData, d => d.bachelorsOrHigher);
+            const maxEData = d3.max(eData, d => d.bachelorsOrHigher);
+
+            const colorScheme = d3.scaleQuantize()
+                                    .domain([minEData,maxEData])
+                                    .range(d3.schemeReds[7]);
+
+            const educationMap = {};
+            eData.forEach(d => educationMap[d.fips] = d.bachelorsOrHigher)
 
 //Append map to svg
                 svg.selectAll("path")
@@ -27,7 +43,8 @@
                     .attr("fill", "white")
                     .attr("stroke", "black")
                     .attr("stroke-width", 0.2)
-                    .attr("transform", "translate(100, 10) scale(0.8)");
+                    .attr("transform", "translate(100, 10) scale(0.8)")
+                    .attr("fill", d => colorScheme(educationMap[d.id]));
 
         }
             catch(error){
