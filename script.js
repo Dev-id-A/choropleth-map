ea0b689cc8f1bdd4c564066210b234949d4c1520
Dev-id-A <davidaranzana618@gmail.com>
2025-06-17 13:21:46 +0200

    Legend created

--- script.js
+++ script.js
@@ -31,14 +31,18 @@ Promise.all([
                                     .range(d3.schemeReds[7]);
 
             const educationMap = {};
-            eData.forEach(d => educationMap[d.fips] = d.bachelorsOrHigher)
+            eData.forEach(d => educationMap[d.fips] = d.bachelorsOrHigher);
 
 //Append map to svg
                 svg.selectAll("path")
                     .data(counties)
                     .enter()
                     .append("path")
+                    .attr("data-fips", d => d.id)
+                    .attr("data-education", d => educationMap[d.id])
+                    .attr("class", "county")
                     .attr("d", d3.geoPath())
+
 //Styling map
                     .attr("fill", "white")
                     .attr("stroke", "black")
@@ -46,9 +50,31 @@ Promise.all([
                     .attr("transform", "translate(100, 10) scale(0.8)")
                     .attr("fill", d => colorScheme(educationMap[d.id]));
 
+
+//Legend
+                const colorThresholds = colorScheme.thresholds();
+                const legendHeight = 20;
+                const legendWidth = 20
+
+                const legend = svg.append("g")
+                    .attr("id", "legend")
+                    .attr("transform", "translate(620, 50)");
+
+                legend.selectAll("rect")
+                    .data(colorScheme.range())
+                    .enter()
+                    .append("rect")
+                    .attr("height", legendHeight)
+                    .attr("width", legendWidth)
+                    .attr("x", (d, i) => i * legendWidth)
+                    .attr("y", 0)
+                    .attr("fill", d => d)
+                    .attr("stroke", "black")
+                    .attr("stroke-width", 0.3);
+
         }
             catch(error){
-            window.alert("There was an error trying to obtain data.");
+            window.alert("There was an error trying to obtain data." + error);
         };    
 });
 
